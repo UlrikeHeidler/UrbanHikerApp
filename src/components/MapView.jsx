627594fa9f982593f2b29c9784e0662f2101efa9
Ulrike Heidler <ulrike.heidler.biz@gmail.com>
2026-07-22 10:56:09 -0400

    Adding a few security fixes to the index.html file, including a Content Security Policy (CSP) to restrict the sources of scripts, styles, images, and connections.

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -89,7 +89,7 @@ function TransitPopup({ node, label, open }) {
  */
 function makeColorIcon(color) {
   return new L.Icon({
-    iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`,
+    iconUrl: `/markers/marker-icon-2x-${color}.png`,
     shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
     iconSize: [25, 41], iconAnchor: [12, 41], popupAnchor: [1, -34], shadowSize: [41, 41],
   })
